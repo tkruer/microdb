67f672e47e565c1db6c60b5d3f32ad9e93304ab4
Tyler Kruer <tylerkruer1@gmail.com>
2025-04-03 11:17:44 -0700

    rewrote for demo and write up

---
+++ build/CMakeFiles/db.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/db.dir/src/main.cpp.o"
+  "CMakeFiles/db.dir/src/main.cpp.o.d"
+  "db"
+  "db.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/db.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use microdb::MicroDB;
-use std::env;
-
-fn main() {
-    let mut database = MicroDB::new().builder(env::args().collect());
-
-    database.run();
-}
